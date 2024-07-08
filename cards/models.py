940329daaaa952ca075b2052664f85fc64f28443
Amir Ghasemian <amirghasemian1381@gmail.com>
2024-07-08 18:40:19 +0330

    i've added a function for moving the card between the boxes and if we recall it , we can move the box forward and if we don't recall it we move card a level back

--- cards/models.py
+++ cards/models.py
@@ -16,5 +16,22 @@ class Card(models.Model):
     box = models.IntegerField(choices=zip(BOXES, BOXES), default=BOXES[0])
     date_created = models.DateTimeField(auto_now_add=True)
     
+    
+    def move(self, solved):
+        """
+        this function is for checking the user recall the answer of the question or not 
+        if user recall it then we can move the card forward and if the user does not know and does not 
+        recall the answer we move the card back to the first box...
+        Args:
+            solved (bool): this is for cheking the user recall it or not
+        """
+        new_box = self.box + 1 if solved else self.box - 1
+        
+        if new_box in BOXES:
+            self.box = new_box
+            self.save()
+        return self
+        
+    
     def __str__(self) -> str:
         return self.question
