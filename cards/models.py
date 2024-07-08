46efdb84c157973f96850169fd3f18ddba0192b1
Amir Ghasemian <amirghasemian1381@gmail.com>
2024-07-08 10:40:03 +0330

    i've created a model for handling the cards that will hold the questions and answers of the user

--- cards/models.py
+++ cards/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+NUM_BOXES = 5
+BOXES = range(1, NUM_BOXES + 1)
+
+class Card(models.Model):
+    """
+    this class is a model for showing the card and make answer and question for it
+
+    Args:
+        models (_type_): _description_
+    """
+    question = models.CharField(max_length=100)
+    answer = models.CharField(max_length=100)
+    box = models.IntegerField(choices=zip(BOXES, BOXES), default=BOXES[0])
+    date_created = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self) -> str:
+        return self.question
