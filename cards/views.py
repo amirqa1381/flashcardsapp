e63eae967ef689c0b16fd1024f31f956aff52e2a
Amir Ghasemian <amirghasemian1381@gmail.com>
2024-07-08 18:07:17 +0330

    here i've created two class that is one of them for creating new card and another one is for updating card

--- cards/views.py
+++ cards/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView, UpdateView
 from .models import Card
+from django.urls import reverse_lazy
 
 
 
@@ -10,3 +11,28 @@ class CardListView(ListView):
     ordering = ['box', '-date_created']
     template_name = "cards/card_list.html"
 
+
+
+class CreateCardView(CreateView):
+    """
+    this class is for creating the card for the user , actually user can add card for him or her self
+    and work and train 
+
+    Args:
+        CreateView (view): this is a class that we inherite form it
+    """
+    model = Card
+    fields = ["question", "answer", "box"]
+    success_url = reverse_lazy("create-card")
+    template_name = "cards/card_form.html"
+    
+    
+class UpdateCardView(CreateCardView,UpdateView):
+    """
+    this class is for updating the card info , and here i inherite form the createcardview because fields 
+    of the createcardview and updatecardview are the same
+    Args:
+        UpdateView (view): this is the class that we inherite from it
+    """
+    success_url = reverse_lazy("card-list")
+    
