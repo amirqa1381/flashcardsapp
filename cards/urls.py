4665ce71a2dbef2bd3f225f83bdbe21beabce856
Amir Ghasemian <amirghasemian1381@gmail.com>
2024-07-08 18:06:36 +0330

    Added two route to it , one of them is new and another one is edit and update

--- cards/urls.py
+++ cards/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import CardListView
+from .views import CardListView, CreateCardView, UpdateCardView
 
 urlpatterns = [
-    path('', CardListView.as_view(), name='card-list')
+    path('', CardListView.as_view(), name='card-list'),
+    path("new/", CreateCardView.as_view(), name="create-card"),
+    path("edit/<int:pk>/", UpdateCardView.as_view(), name="card-update")
 ]
